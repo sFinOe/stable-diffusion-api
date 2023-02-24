71479a31a72e164cd2a5915a0e0b8157977eee84
Zakaria Kasmi <zakkikasmi@gmail.com>
2023-02-24 14:46:57 +0100

    v2

--- v1/test.py
+++ v1/test.py
@@ -3,9 +3,9 @@ from botocore.client import Config
 from botocore.exceptions import ClientError
 
 s3 = boto3.resource('s3',
-                    endpoint_url='https://c5496cc41ca6d42c8358101ad551f1b4.r2.cloudflarestorage.com',
-                    aws_access_key_id='4e57071dba2f0fb9f5a2af8037e95e82',
-                    aws_secret_access_key='0bdd57295ce8d381cb6a9ab486a0f02abbd9ab9eff8a7f521bea7cd03de8189c',
+                    endpoint_url='https://xxxxxxxxxxxxxxxxxxx.r2.cloudflarestorage.com',
+                    aws_access_key_id='xxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+                    aws_secret_access_key='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
                     config=Config(signature_version='s3v4')
                     )
 
